366468c28da1b98d4efb02bcaf11a50369307a0f
vanshikaaa <vanshikhashri@gmail.com>
2026-03-16 10:27:22 +0530

    Time: 2 ms (58.55%) | Memory: 12.7 MB (20.40%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -4,22 +4,12 @@ class Solution(object):
         hashmap={}
         n=len(nums2)
         for i in range(n-1,-1,-1):
-            # print(i,stack)
+            while stack and stack[-1]<=nums2[i]:
+                stack.pop()
             if stack==[]:
-                # res.append(-1)
                 hashmap[nums2[i]]=-1
-            elif nums2[i]<stack[-1]:
-                # res.append(stack[-1])
-                hashmap[nums2[i]]=stack[-1]
             else:
-                while stack and stack[-1]<nums2[i]:
-                    stack.pop()
-                if stack:
-                    # res.append(stack[-1])
-                    hashmap[nums2[i]]=stack[-1]
-                else:
-                    # res.append(-1)
-                    hashmap[nums2[i]]=-1
+                hashmap[nums2[i]]=stack[-1]
             stack.append(nums2[i])
         ans=[]
         for i in range(len(nums1)):
