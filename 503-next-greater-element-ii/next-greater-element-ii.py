fde88b1e01faa7b727e3fd65c7fbab80c510e908
vanshikaaa <vanshikhashri@gmail.com>
2026-03-16 11:18:48 +0530

    Time: 47 ms (34.85%) | Memory: 14.2 MB (66.31%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.py
+++ 503-next-greater-element-ii/next-greater-element-ii.py
@@ -1,29 +1,16 @@
-class Solution:
-    def nextGreaterElements(self, nums: List[int]) -> List[int]:
-        stack=[]
-        hashmap={}
-        res=[]
+class Solution(object):
+    def nextGreaterElements(self, nums):
         n=len(nums)
-        for i in range(n-1,-1,-1):
-            if stack==[]:
-                res.append(-1)
-            elif nums[i]<stack[-1]:
-                res.append(stack[-1])
-            else:
-                while stack and stack[-1]<=nums[i]:
-                    stack.pop()
-                if stack:
-                    res.append(stack[-1])
+        stack=[]
+        ans=[]
+        for i in range(2*n-1,-1,-1):
+            while stack and stack[-1]<=nums[i%n]:
+                stack.pop()
+            if i<n:
+                if stack==[]:
+                    ans.append(-1)
                 else:
-                    res.append(-1)
-            stack.append(nums[i])
-        res=res[::-1]
-        for i in range(n):
-            if res[i]==-1:
-                j=(i+1)%n
-                while j!=i:
-                    if nums[j]>nums[i]:
-                        res[i]=nums[j]
-                        break
-                    j=(j+1)%n
-        return res
+                    ans.append(stack[-1])
+            stack.append(nums[i%n])
+        return ans[::-1]
+        
