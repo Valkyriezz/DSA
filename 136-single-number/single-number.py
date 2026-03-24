d1557f53e1688bf420eaf8c057700b56d77427d5
vanshikaaa <vanshikhashri@gmail.com>
2026-03-24 12:51:26 +0530

    Time: 0 ms (100.00%) | Memory: 21.3 MB (41.60%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,24 +1,9 @@
-class Solution(object):
-    def singleNumber(self, nums):
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        xor=nums[0]
         n=len(nums)
-        # for i in range(n):
-        #     num=nums[i]
-        #     count=0
-        #     for j in range(n):
-        #         if num==nums[j]:
-        #             count+=1
-        #     if count==1:
-        #         return num
-        # hashmap={}
-        # for i in range(n):
-        #     if nums[i] in hashmap:
-        #         hashmap[nums[i]]+=1
-        #     else:
-        #         hashmap[nums[i]]=1
-        # for key,val in hashmap.items():
-        #     if val==1:
-        #         return key
-        xor=0
-        for i in range(n):
-            xor=xor^nums[i]
+        for i in range(1,n):
+            xor^=nums[i]
         return xor
+
+        
